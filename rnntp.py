3f0dd4046acc5c635454de790831b3d4e8c67f5d
Will Mildenhall <wtmildenhall@gmail.com>
2019-03-08 14:04:33 -0700

    getting tensorflow and keras loaded

---
+++ rnntp.py
@@ -0,0 +1,25 @@
+import numpy as np
+np.random.seed(42)
+import tensorflow as tf
+tf.set_random_seed(42)
+from keras.models import Sequential, load_model
+from keras.layers import Dense, Activation
+from keras.layers import LSTM, Dropout
+from keras.layers import TimeDistributed
+from keras.layers.core import Dense, Activation, Dropout, RepeatVector
+from keras.optimizers import RMSprop
+import matplotlib.pyplot as plt
+import pickle
+import sys
+import heapq
+import seaborn as sns
+from pylab import rcParams
+
+sns.set(style='whitegrid', palette='muted', font_scale=1.5)
+
+rcParams['figure.figsize'] = 12, 5
+
+#need to fine the text, or our own text to use
+#path = 'nietzsche.txt'
+#text = open(path).read().lower()
+#print('corpus length:', len(text))
